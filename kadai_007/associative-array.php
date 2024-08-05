a91022304123b110d0faf98b8970c6aa0d954587
ka-ho-ri <126395705+ka-ho-ri@users.noreply.github.com>
2024-08-05 23:00:42 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charaset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+
+    <?php
+
+$vegi = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+
+print_r($vegi);
+
+?>
+</p>
+</body>
+</html>
